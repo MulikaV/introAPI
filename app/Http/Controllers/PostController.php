ff5e4cd5b535861bdb498812835b7ec3c946c68d
mulikav <mulikavj@gmail.com>
2020-02-21 15:36:41 +0200

    Basic_api_refactor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,32 +11,33 @@ class PostController extends Controller
 
     public function index(Request $request)
     {
-        $sortBy = $request->input('sortBy');
+        $sortBy = $request->input('sortBy', null);
+        $order = $request->input('order', null);
 
-        if (isset($sortBy)) {
-            $order = $request->input('order');
-            if ($order && $order == 'desc') {
-                return Post::all()->sortByDesc($sortBy)->values();
-            } else {
-                return Post::all()->sortBy($sortBy)->values();
-            }
-        } else {
-            return Post::all();
+        $posts = Post::with('user');
+
+        if ($sortBy && $order) {
+            $posts->orderBy($sortBy, $order);
+        } else if ($sortBy) {
+            $posts->orderBy($sortBy);
         }
+
+        return $posts->get();
+
     }
 
     public function store(Request $request)
     {
 
-        $post = Post::add($request->toArray());
+        $post = Post::create($request->toArray());
         return response()->json($post, 201);
     }
 
     public function update(Request $request, $id)
     {
         $post = Post::find($id);
-        $post->edit($request->toArray());
-        return response()->json($post, 201);
+        $post->update($request->toArray());
+
     }
 
     public function show($id)
@@ -46,6 +47,6 @@ class PostController extends Controller
 
     public function destroy($id)
     {
-        Post::find($id)->remove();
+        Post::find($id)->delete();
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
@@ -11,26 +10,8 @@ class Post extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class);
     }
 
-    public static function add($fields)
-    {
-        $post = new static();
-        $post->fill($fields);
-        $post->save();
-        return $post;
-    }
-
-    public function edit($fields)
-    {
-        $this->fill($fields);
-        $this->save();
-    }
-
-    public function remove()
-    {
-        $this->delete();
-    }
 
 }
